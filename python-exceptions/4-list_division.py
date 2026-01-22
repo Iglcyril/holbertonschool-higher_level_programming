fac71bda20026b132b7a39e543ffb4dfa3e2c60e
Iglcyril <iglcyril@hotmail.fr>
2026-01-22 13:47:39 +0000

    feat: add initial implementation of addition functionality

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except TypeError:
             print("wrong type")
+			result.append(0)
             
         except ZeroDivisionError:
             print("division by 0")
