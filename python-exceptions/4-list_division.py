c6784d1951c9d94bb5ee29888b6c0f59e2185c5e
Iglcyril <iglcyril@hotmail.fr>
2026-01-22 13:50:24 +0000

    fix: handle exceptions in list_division function for ZeroDivisionError and IndexError

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,9 +13,11 @@ def list_division(my_list_1, my_list_2, list_length):
             
         except ZeroDivisionError:
             print("division by 0")
+			result.append(0)
             
         except IndexError:
             print("out opf range")
+			result.append(0)
             
         finally:
             pass
