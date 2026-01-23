c46c2b97a91a71287f7967c86fff3c65539c873b
Iglcyril <iglcyril@hotmail.fr>
2026-01-23 08:02:27 +0000

    fix: update docstring for add_integer function to improve clarity and detail

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+    Additionne deux entiers.
+    
+    Args:
+        a (int, float): Premier nombre
+        b (int, float): Deuxième nombre (défaut: 98)
+    
+    Returns:
+        int: La somme de a et b
+    
+    Raises:
+        TypeError: Si a ou b n'est pas un int ou float
+    """
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
