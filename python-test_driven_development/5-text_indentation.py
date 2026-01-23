5fe8f6798361964bdb3bf3569cfe1baff384d148
Iglcyril <iglcyril@hotmail.fr>
2026-01-23 13:46:08 +0000

    feat:feat: add type check for text input in text_indentation function

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
     letter =0
     while letter < len(text):
         print(text[letter], end="")
