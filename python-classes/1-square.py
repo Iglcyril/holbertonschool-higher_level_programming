cfb5226078abb5e823b42d0aabaeddb9aafa5643
Iglcyril <iglcyril@hotmail.fr>
2026-01-26 09:42:15 +0000

    feat: Write a class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class is used to represent square geometric shapes
+and will be used for various calculations in future implementations.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    
+    A square is a geometric shape with four equal sides.
+    This is an empty class that will be expanded later.
+    """
+    def __init__(self, size)
+        """
+        Initialize a new Square instance.
+        
+        Args:
+            size: The size of the square (no type/value verification).
+        """
+
+    self.__size = size
