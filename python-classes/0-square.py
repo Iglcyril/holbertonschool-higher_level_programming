026708b3ebd4678ed7df197b9749323dcf05f091
Iglcyril <iglcyril@hotmail.fr>
2026-01-26 07:52:48 +0000

    feat: add Square class definition with initial docstring

---
+++ python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class is used to represent square geometric shapes
+and will be used for various calculations in future implementations.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    
+    A square is a geometric shape with four equal sides.
+    This is an empty class that will be expanded later.
+    """
+    pass
