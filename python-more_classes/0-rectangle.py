07dab9fe65f310144342c4268106a82de33027f8
Luken <luken@laptop-hmlsaa6l.home>
2026-01-27 10:26:56 +0100

    feat: define Rectangle class as a placeholder for future square implementations

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class is used to represent square geometric shapes
+and will be used for various calculations in future implementations.
+"""
+
+class Rectangle:
+	"""
+    This class represents a square.
+    
+    A square is a geometric shape with four equal sides.
+    This is an empty class that will be expanded later.
+    """
+	pass
