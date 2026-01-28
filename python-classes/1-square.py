cbdeecb3ea23723237d6796c0bba2e427b868f22
Luken <luken@laptop-hmlsaa6l.home>
2026-01-28 10:01:03 +0100

    fix: some corrections for pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+
 """
 This module defines a Square class.
 
@@ -11,16 +11,16 @@ and will be used for various calculations in future implementations.
 class Square:
     """
     This class represents a square.
-    
+
     A square is a geometric shape with four equal sides.
     This is an empty class that will be expanded later.
     """
     def __init__(self, size):
         """
         Initialize a new Square instance.
-        
+
         Args:
             size: The size of the square (no type/value verification).
         """
 
-    self.__size = size
+        self.__size = size
