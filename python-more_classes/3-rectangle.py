e64dd6f09e4998909d62b7d06d3550c1c63ec2d3
Luken <luken@laptop-hmlsaa6l.home>
2026-01-28 11:34:13 +0100

    fix: correction error message on setter for the height

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -59,7 +59,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
