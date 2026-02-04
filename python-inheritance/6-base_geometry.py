3c1bc089ef85d44d3ce7c7f3d28c66d58a354586
Luken <luken@laptop-hmlsaa6l.home>
2026-02-04 09:21:22 +0100

    feat: define BaseGeometry class as a foundation for geometry-related classes

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,14 +7,14 @@ This module defines an empty BaseGeometry class.
 class BaseGeometry:
     """
     Base class for geometry objects.
-    
+
     This class provides a foundation for geometry-related classes.
     """
 
     def area(self):
         """
         Calculate the area.
-        
+
         Raises:
             Exception: This method is not implemented in the base class.
         """
