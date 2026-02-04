4ec14d6686dc4e26de5d415584ee1b2ff741499f
Luken <luken@laptop-hmlsaa6l.home>
2026-02-04 09:10:34 +0100

    feat: define empty BaseGeometry class as a base for geometry-related classes

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that serves as a base for geometry-related classes.
+    """
+    pass
