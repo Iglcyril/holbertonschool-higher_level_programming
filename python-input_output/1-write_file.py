d98e34dcb8cec4545773118251e1d574cb080c61
Luken <luken@laptop-hmlsaa6l.home>
2026-02-09 15:03:34 +0100

    feat: add write_file function to write a string to a text file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns number of characters
+
+    Args:
+        filename: name of the file to write to
+        text: text to write to the file
+
+    Returns:
+        Number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
